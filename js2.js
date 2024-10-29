f9271612dd5ad0b97db8e22da01af3c98eb049fc
Monika-SV <monikavenugopal27@gmail.com>
2024-10-29 22:16:45 +0530

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,11 @@
+
+// Change navbar background color when scrolling
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
